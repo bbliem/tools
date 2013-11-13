4b988624637912cea9dba42cf5baacc967122107
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2013-11-13 16:12:15 +0100

    Added dual screen mode

--- screen.sh
+++ screen.sh
@@ -6,7 +6,9 @@ elif [ "$1" == "lvds" ]; then
 	xrandr --output LVDS1 --auto --output VGA1 --off
 elif [ "$1" == "mirror" ]; then
 	xrandr --output LVDS1 --mode 1024x768 --output VGA1 --mode 1024x768
+elif [ "$1" == "dual" ]; then
+	xrandr --output LVDS1 --auto --output VGA1 --auto --above LVDS1
 else
-	echo "Usage: $0 vga | lvds | mirror"
+	echo "Usage: $0 vga | lvds | mirror | dual"
 	exit 1
 fi
