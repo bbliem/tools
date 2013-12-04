6f2aff1752aa0953ea82d8af021735201ae87839
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2013-12-04 23:58:40 +0100

    Changed flac2mp3.sh to use ffmpeg in order to retain audio file metadata

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for f in "$@"; do
-	flac -d -c "$f" | lame -V 4 - "${f%%.flac}.mp3"
+#	flac -d -c "$f" | lame -V 4 - "${f%%.flac}.mp3"
+	ffmpeg -i "$f" -qscale:a 0 "${f[@]/%flac/mp3}"
 done
