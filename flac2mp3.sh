19511c48d97bb08741fc2dd23ba5e811dbef07e7
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2013-12-22 00:56:11 +0100

    flac2mp3 is now multi-threaded

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for f in "$@"; do
-#	flac -d -c "$f" | lame -V 4 - "${f%%.flac}.mp3"
-	ffmpeg -i "$f" -qscale:a 0 "${f[@]/%flac/mp3}"
-done
+#for f in "$@"; do
+##	flac -d -c "$f" | lame -V 4 - "${f%%.flac}.mp3"
+#	ffmpeg -i "$f" -qscale:a 0 "${f[@]/%flac/mp3}"
+#done
+
+numProcesses=$(nproc)
+find "$@" -name "*.flac" | xargs -P $numProcesses -n 1 -I{} bash -c 'f="{}"; ffmpeg -i "$f" -qscale:a 0 "${f[@]/%flac/mp3}"'
